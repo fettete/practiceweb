af670d3ea9d886862435f9d32810df7e0fe78d68
fettete <100012282+fettete@users.noreply.github.com>
2022-02-19 13:00:06 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").innerHTML = "Aidoshka CS-2119";
+function display()
+{
+  document.getElementById('date').innerHTML = Date();
+}
+document.querySelector("h3").innerHTML = "876 days left until the freedom!"
+function multiplyBy() {
+        num1 = document.getElementById("1Num").value;
+        num2 = document.getElementById("2Num").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() {
+        num1 = document.getElementById("1Num").value;
+        num2 = document.getElementById("2Num").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
